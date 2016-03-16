0dae8ceabcc6565a530813aea515f075beb70b38
shmulim <shmuel.markel@gmail.com>
2016-03-16 00:09:15 -0700

    Done.

--- spec/script_spec.js
+++ spec/script_spec.js
@@ -35,13 +35,13 @@ describe('#passBill', function() {
 describe('#doCharity', function() {
 
 
-  it('returns "I like the help people."', function() {
+  it('returns "I like to help people."', function() {
     var lyndonJohnson = new President("Lyndon B Johnson", "Democrat", "1963-1969", "Texas");
-    expect(lyndonJohnson.doCharity()).toEqual("I like the help people.");
+    expect(lyndonJohnson.doCharity()).toEqual("I like to help people.");
   });
 });
 
-describe('#onductPressInterview', function() {
+describe('#conductPressInterview', function() {
 
 
   it('returns "I am proud to be an American."', function() {
